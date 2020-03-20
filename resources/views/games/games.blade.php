c13a00a7cfe5a17b99eb50980e66eda9fade378c
letsch <eduardoletsch@hotmail.com>
2020-03-19 22:31:25 -0300

    CSSing the website

--- resources/views/games/games.blade.php
+++ resources/views/games/games.blade.php
@@ -4,22 +4,32 @@
 <h1 style="text-align: center"> Games List </h1>
 <hr style="margin-left:45%; margin-right: 45%; border-top: 2px solid black">
 
-<div style="display: flex; justify-content: space-evenly;">
-    
-    @foreach($games as $game)
-        <div class="card">
-            <p> {{ $game->name }} </p>
-            <p> {{ $game->type }} </p>
-            <p> {{ $game->price }} </p>
-        </div>
-    @endforeach 
-
-    <!--@for($i = 0; $i < count($games); $i++)
-        <div>
-        <p> {{ $games[$i]['name'] }} </p>
-        <p> {{ $games[$i]['type'] }} </p>
-        <p> {{ $games[$i]['price'] }} </p>
-        </div>
-    @endfor -->
+<div style="text-align: center; margin-bottom: 1%">
+    <a class="btn btn-dark" href="/create" role="button">Create Game</a>
 </div>
+   
+<div class="container">
+    <div class="row">
+        @foreach($games as $game)
+            <div class="col-sm-4"> 
+                <div class="card" style="margin-bottom: 5%">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $game->name }}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted"> ${{ $game->price }}</h6>
+                        <p class="card-text">{{ $game->type }}</p>
+                        <a class="btn btn-dark" href="{{ URL::current() . '/' . $game->id }}" role="button">See More</a>
+                    </div>
+                </div>      
+            </div>
+        @endforeach 
+    </div>
+</div>
+<!--@for($i = 0; $i < count($games); $i++)
+    <div>
+    <p> {{ $games[$i]['name'] }} </p>
+    <p> {{ $games[$i]['type'] }} </p>
+    <p> {{ $games[$i]['price'] }} </p>
+    </div>
+@endfor -->
+
 @endsection
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,74 +1,102 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>Laravel</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <style>
+        html,
+        body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .full-height {
+            height: 100vh;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .title {
-                font-size: 84px;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+        .content {
+            text-align: center;
+        }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        @yield('content')
+        .title {
+            font-size: 84px;
+        }
+
+        .links>a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="margin-bottom: 1%; padding-left: 10%; padding-right: 10%">
+        <a class="navbar-brand" href="/">Games Locator</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            </ul>
+
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
 
 
-        <footer style="background: #eee; padding: 5%; text-align: center"> 
-            <p>Copyright 2020 Games Locator</p>
-        </footer>
-    </body>
+    @yield('content')
+
+
+    <footer style="background: #343a40; padding: 5%; text-align: center">
+        <div style="color: white">    
+            <p>Made with <3 by Eduardo Letsch</p>
+        </div>
+    </footer>
+</body>
+
 </html>
